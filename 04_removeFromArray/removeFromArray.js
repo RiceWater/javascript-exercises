cd31024ce89c27c4f57b4bfb78e3ea104c18e6d4
Will <willianchua.wc@gmail.com>
2023-12-23 19:20:02 +0800

    Complete Item 4
    I applied One purpose for functions which made it simple and easy to understand. I also checked the output of the test to identify what went wrong. Lastly, I learned about the arguments object in functions.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,18 @@
-const removeFromArray = function(givenArray, numRemove) {
+const removeFromArray = function() {
+    for (let i = 1; i < arguments.length; i++){
+        arguments[0] = deleteElement(arguments[0], arguments[i]);
+    }
+    return arguments[0];
+};
+
+function deleteElement(givenArray, numRemove){
     for (let i = 0; i < givenArray.length; i++){
-        if (givenArray[i] == numRemove){
+        if (givenArray[i] === numRemove){
             givenArray.splice(i,1);
         }
     }
     return givenArray;
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
