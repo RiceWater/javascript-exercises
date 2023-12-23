a263456df4a59c57ce9de2e49b2537de3f7c1c4f
Will <willianchua.wc@gmail.com>
2023-12-23 19:06:30 +0800

    Solve first test case
    I was going to separate the array, pop using pop(), then concatenate them together, but then I found out that array splice pops an element and does not leave undefined.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, numRemove) {
+    for (let i = 0; i < givenArray.length; i++){
+        if (givenArray[i] == numRemove){
+            givenArray.splice(i,1);
+        }
+    }
+    return givenArray;
 };
 
 // Do not edit below this line
