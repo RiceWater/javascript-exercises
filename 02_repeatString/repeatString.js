5a22afa53bfe466d093de72558a52b5b82448340
Will <willianchua.wc@gmail.com>
2023-12-23 18:36:47 +0800

    Answer Item 2
    I read the documentation on how concat() works. It does not update the variable the called the function. The updated string is the return value. Also, I used Number() for conversion and added code to check if the input is nonnegative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (Number(num) < 0){
+        return 'ERROR';
+    }
 
+    let output = '';
+    for (let i = 0; i < Number(num); i++){
+        output = output.concat(str);
+    }
+    return output;
 };
 
 // Do not edit below this line
