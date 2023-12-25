de1db68b05e5079790dcf4526d96f8923b7ba14a
Will <willianchua.wc@gmail.com>
2023-12-25 15:49:16 +0800

    Complete Item 8
    i realized that test cases with multiple inputs were passed as inside an array instead of separate arguments. So, I recoded so it can work with arrays. In the solution though, they used array.reduce(), which led me to researching what it does.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(a => { sum += a; });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  arr.forEach(a => { prod *= a; });
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let prod = 1;
+  for (let i = 1; i <= num; i++){
+    prod *= i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
