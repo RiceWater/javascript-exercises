712ffb7afeba527a8911b5e0646d0403d252b7bc
Will <willianchua.wc@gmail.com>
2023-12-25 16:20:48 +0800

    Complete Item 9
    This item was okay. I was able to make my functions simple and readable, but I was amazed as to how little lines of code you needed for this problem. The solution only had 2 lines of code. Not only that, it was easy to understand as well.
    
    This shows how much I need to be familiar with JS functions.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,51 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    words = words.toLowerCase();
+    let left = 0, right = words.length - 1;
+    while (left < right){
+        left = reAdjustLeftSide(words, left);
+        right = reAdjustRightSide(words, right);
+        if (words[left] != words[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 
 };
+
+function reAdjustLeftSide(words, left){
+    while(true){
+        if (!isAlphanumeric(words.charCodeAt(left))){
+            left++;
+        }
+        else{
+            break;
+        }
+    }
+    return left;
+}
+
+function reAdjustRightSide(words, right){
+    while(true){
+        if (!isAlphanumeric(words.charCodeAt(right))){
+            right--;
+        }
+        else{
+            break;
+        }
+    }
+    return right;
+}
+
+function isAlphanumeric(code){
+    if (!(code > 47 && code < 58) &&    // 0 to 9
+        !(code > 64 && code < 91) &&    // A to Z
+        !(code > 96 && code < 123)){    // a - z
+        return false;
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
