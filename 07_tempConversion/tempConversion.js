200e959b30c4182fd1df7f19932504624baedc4f
Will <willianchua.wc@gmail.com>
2023-12-25 15:33:08 +0800

    Complete Item 7
    I completed it and it was fairly easy. But, I learned that the toFixed() function returns a STRING and not a float. So, I used Number functions to fix this

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let cels = (fahren - 32) * 5 / 9;
+  if (Number.isInteger(cels))
+    return cels;
+  return Number.parseFloat(cels.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let fahren = (cels * 9 / 5) + 32; 
+  if (Number.isInteger(fahren)) 
+    return fahren;
+  return Number.parseFloat(fahren.toFixed(1));
 };
 
 // Do not edit below this line
