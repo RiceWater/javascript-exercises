6210b8cc4ddde892453b7fb106e68e0980b20ab8
Will <willianchua.wc@gmail.com>
2023-12-26 13:29:07 +0800

    Complete Item 10 with second method
    Yep, there exists a formula for it. I checked if Math.Trunc or Math.Round would work and .Round makes the correct output.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,13 +3,10 @@ const fibonacci = function(num) {
     if (num < 1) return 'OOPS';
     if (num == 1 || num == 2) return 1;
 
-    let prev = 1, curr = 1, tmp = 0;
-    for (let i = 3; i <= num; i++){
-        tmp = curr;
-        curr += prev;
-        prev = tmp;
-    }
-    return curr;
+    const FIVESQRT = Math.sqrt(5);
+    const PHI = (FIVESQRT + 1) / 2;
+    
+    return Math.round((PHI ** num) / FIVESQRT);
 };
 
 // Do not edit below this line
