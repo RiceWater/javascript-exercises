dfa1e1fd64d84b20109bbe18742627445bb79e2c
Will <willianchua.wc@gmail.com>
2023-12-26 13:23:51 +0800

    Complete Item 10
    It was easy since it is about understanding the problem and its expected outputs. Though, I searched the formula for computing using PHI, I have yet to test it.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number.parseInt(num);
+    if (num < 1) return 'OOPS';
+    if (num == 1 || num == 2) return 1;
 
+    let prev = 1, curr = 1, tmp = 0;
+    for (let i = 3; i <= num; i++){
+        tmp = curr;
+        curr += prev;
+        prev = tmp;
+    }
+    return curr;
 };
 
 // Do not edit below this line
