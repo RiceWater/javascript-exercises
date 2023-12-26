301b0b3368c77a0aa7000df1938eddb8c2f02bc0
Will <willianchua.wc@gmail.com>
2023-12-26 14:20:02 +0800

    Complete Item 12
    I had trouble with this. I knew that the strightforward approach would solve it easily, so I wanted to use the reduce function. I added an age attribute for tracking with ease, but the problem was building the reduce function.
    
    It was simple, yet I overlooked it. I learned that if there are braces after the lambda function, I need to put the return keyword. Otherwise, I can omit it. No wonder the reduce function kept on returning undefined. Also, the reduce function can handle objects.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    for (let i = 0; i < persons.length; i++){
+        persons[i].age = getAge(persons[i]);
+    }
+    return persons.reduce((acc, cv) => (acc.age < cv.age) ? cv : acc);
 };
+
+function getAge(person){
+    if (person.yearOfDeath != undefined){
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    return new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
